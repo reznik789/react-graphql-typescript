0e178996265fcdf17281a9f69f2e218fc1745b8f
Alex Reznichenko <alex.r@itmaster-soft.com>
2021-10-23 20:35:11 +0300

    DB collections and seed functions

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,15 +1,15 @@
 import { MongoClient } from "mongodb";
-import { Database } from "../lib/types";
+import { Booking, Database, Listing, User } from "../lib/types";
 
-const url = `mongodb+srv://${process.env.DB_USER}:${
-  process.env.DB_USER_PASSWORD
-}@${process.env.DB_CLUSTER}.mongodb.net`;
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net`;
 
 export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(url, { useNewUrlParser: true });
   const db = client.db("main");
 
   return {
-    listings: db.collection("test_listings")
+    listings: db.collection<Listing>("listings"),
+    users: db.collection<User>("users"),
+    bookings: db.collection<Booking>("bookings"),
   };
 };
